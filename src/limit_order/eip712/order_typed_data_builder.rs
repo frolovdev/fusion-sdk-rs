9cd27477a49f740279eb5c80e090bc75702ce8f4
frolovdev <frolovandrey752@gmail.com>
2023-04-07 09:23:22 +0400

    eip712

--- src/limit_order/eip712/order_typed_data_builder.rs
+++ src/limit_order/eip712/order_typed_data_builder.rs
@@ -1,40 +1,81 @@
+use ethers::abi::AbiEncode;
+use ethers::types::transaction::eip712::{hash_struct, Eip712, TypedData};
 use ethers::types::transaction::eip712::{EIP712Domain, Eip712DomainType};
-use ethers::types::transaction::eip712::{TypedData};
 use ethers::types::{H160, U256};
+use serde_json::json;
 use std::collections::BTreeMap;
 use std::str::FromStr;
 
-use crate::limit_order::types::LimitOrderV3Struct;
+use crate::constants::ONE_INCH_ROUTER_V5;
+use crate::limit_order::types::{LimitOrderV3Struct, ToBtreeMap};
 
-use super::domain::{eip712_domain_type, order_type};
-
-pub fn get_order_hash() {}
+use super::domain::{eip712_domain_type, order_type, LIMIT_ORDER_V3_TYPE_DATA_NAME, LIMIT_ORDER_V3_TYPE_DATA_VERSION};
 
 pub fn build_order_data(
-    chainId: &U256,
+    chain_id: &U256,
     verifying_contract: &str,
     name: &str,
     version: &str,
     order: LimitOrderV3Struct,
 ) -> TypedData {
     let domain = EIP712Domain {
-        chain_id: Some(chainId.clone()),
+        chain_id: Some(chain_id.clone()),
         verifying_contract: Some(H160::from_str(verifying_contract).unwrap()),
         name: Some(name.to_string()),
         version: Some(version.to_string()),
         salt: None,
     };
-    let types: BTreeMap<String, Vec<Eip712DomainType>> = BTreeMap::new();
+    let mut types: BTreeMap<String, Vec<Eip712DomainType>> = BTreeMap::new();
 
     types.insert("EIP712Domain".to_string(), eip712_domain_type());
     types.insert("Order".to_string(), order_type());
 
-    let message: BTreeMap<String, serde_json::Value> = BTreeMap::new();
+    let message = order.to_btree_map();
 
     TypedData {
         primary_type: "Order".to_string(),
         types,
         domain,
-        message: order,
+        message,
+    }
+}
+
+pub fn get_order_hash(data: TypedData) -> String {
+    "0x".to_string() + &data.encode_eip712().unwrap().encode_hex()
+}
+
+pub fn domain_separator(
+    name: &str,
+    version: &str,
+    chain_id: &U256,
+    verifying_contract: &str,
+) -> String {
+    let mut types: BTreeMap<String, Vec<Eip712DomainType>> = BTreeMap::new();
+
+    types.insert("EIP712Domain".to_string(), eip712_domain_type());
+
+    "0x".to_string()
+        + &hash_struct(
+            "EIP712Domain",
+            &json!({
+                "name": name,
+                "version": version,
+                "chainId": chain_id,
+                "verifyingContract": verifying_contract
+            }),
+            &types,
+        )
+        .unwrap()
+        .encode_hex()
+}
+
+
+pub fn get_limit_order_v3_domain(chain_id: &U256) -> EIP712Domain {
+    EIP712Domain {
+        chain_id: Some(chain_id.clone()),
+        verifying_contract: Some(H160::from_str(ONE_INCH_ROUTER_V5).unwrap()),
+        name: Some(LIMIT_ORDER_V3_TYPE_DATA_NAME.to_string()),
+        version: Some(LIMIT_ORDER_V3_TYPE_DATA_VERSION.to_string()),
+        salt: None,
     }
 }
--- src/limit_order/types.rs
+++ src/limit_order/types.rs
@@ -1,5 +1,11 @@
-use ethers::types::transaction::eip712::{Eip712, EIP712_DOMAIN_TYPE_HASH};
+use std::collections::BTreeMap;
 
+use ethers::types::transaction::eip712::{Eip712, EIP712_DOMAIN_TYPE_HASH};
+use serde::ser::{Serialize, SerializeStruct, Serializer};
+use serde_json::json;
+use struct_field_names_as_array::FieldNamesAsArray;
+
+#[derive(FieldNamesAsArray, Debug)]
 pub struct LimitOrderV3Struct {
     salt: String,
     maker_asset: String,
@@ -13,6 +19,67 @@ pub struct LimitOrderV3Struct {
     interactions: String,
 }
 
-pub fn kekek() {
-    // let kek = Eip712::type_hash() {
+pub trait ToBtreeMap {
+    fn to_btree_map(&self) -> BTreeMap<String, serde_json::Value>;
+}
+
+impl ToBtreeMap for LimitOrderV3Struct {
+    fn to_btree_map(&self) -> BTreeMap<String, serde_json::Value> {
+        let mut map = BTreeMap::new();
+
+        map.insert("salt".to_string(), json!(self.salt));
+        map.insert("makerAsset".to_string(), json!(self.maker_asset));
+        map.insert("taker_asset".to_string(), json!(self.taker_asset));
+        map.insert("maker".to_string(), json!(self.maker));
+        map.insert("receiver".to_string(), json!(self.receiver));
+        map.insert("allowed_sender".to_string(), json!(self.allowed_sender));
+        map.insert("making_amount".to_string(), json!(self.making_amount));
+        map.insert("taking_amount".to_string(), json!(self.taking_amount));
+        map.insert("offsets".to_string(), json!(self.offsets));
+        map.insert("interactions".to_string(), json!(self.interactions));
+
+        if map.len() != LimitOrderV3Struct::FIELD_NAMES_AS_ARRAY.len() {
+            panic!("Not all fields were serialized");
+        }
+
+        map
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn success() {
+        let limit_order = LimitOrderV3Struct {
+            salt: "kek".to_string(),
+            maker_asset: "kek".to_string(),
+            taker_asset: "kek".to_string(),
+            maker: "kek".to_string(),
+            receiver: "kek".to_string(),
+            allowed_sender: "kek".to_string(),
+            making_amount: "kek".to_string(),
+            taking_amount: "kek".to_string(),
+            offsets: "kek".to_string(),
+            interactions: "kek".to_string(),
+        };
+
+        let map = limit_order.to_btree_map();
+
+        let expectedMap = BTreeMap::from([
+            ("salt".to_string(), json!("kek")),
+            ("makerAsset".to_string(), json!("kek")),
+            ("taker_asset".to_string(), json!("kek")),
+            ("maker".to_string(), json!("kek")),
+            ("receiver".to_string(), json!("kek")),
+            ("allowed_sender".to_string(), json!("kek")),
+            ("making_amount".to_string(), json!("kek")),
+            ("taking_amount".to_string(), json!("kek")),
+            ("offsets".to_string(), json!("kek")),
+            ("interactions".to_string(), json!("kek")),
+        ]);
+
+        assert_eq!(map, expectedMap);
+    }
 }
